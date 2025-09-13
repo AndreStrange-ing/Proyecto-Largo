6f91f696ff3a71fecf905f47fcdc699f1be4a34d
AndreStrange-ing <herrera.johann2@gmail.com>
2025-09-13 15:20:40 -0600

    primera correccion para atributos privados

--- proyecto.py
+++ proyecto.py
@@ -2,28 +2,28 @@ from abc import ABC, abstractmethod
 
 class Usuario(ABC):
     def __init__(self, nombre, id_usuario):
-        # Encapsulamiento, atributos protegidos
-        self._nombre = nombre
-        self._id_usuario = id_usuario
-        print(f"Usuario creado: {self._nombre}")
+        # Encapsulamiento, atributos privados
+        self.__nombre = nombre
+        self.__id_usuario = id_usuario
+        print(f"Usuario creado: {self.__nombre}")
 
     @abstractmethod
     def mostrar_info(self):
         # Polimorfismo, se implementará distinto en cada subclase
         pass
 
-    # Getter y Setter para encapsulamiento
+    # Getters y setters
     def get_nombre(self):
-        return self._nombre
+        return self.__nombre
 
     def set_nombre(self, nuevo_nombre):
-        self._nombre = nuevo_nombre
+        self.__nombre = nuevo_nombre
 
     def get_id(self):
-        return self._id_usuario
+        return self.__id_usuario
 
     def __del__(self):
-        print(f"Usuario eliminado: {self._nombre}")
+        print(f"Usuario eliminado: {self.__nombre}")
 
 
 class Estudiante(Usuario):
